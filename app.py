5d92357c2725eaea26e973977847fa6a0d1bdc25
Pawl1973 <pmanaugh@hotmail.com>
2021-11-28 21:32:07 +1100

    Commit 00007

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+db_url = os.environ.get('DATABASE_URL')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("://", "ql://", 1)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'abc'
 api = Api(app)
